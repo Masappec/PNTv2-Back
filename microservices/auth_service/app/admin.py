ff7ec5a9d41b95a4ffdf5cbc08a8015928a480ae
Anderson Sinaluisa <afullink@gmail.com>
2023-12-31 12:18:49 -0500

    fixtures data

--- microservices/auth_service/app/admin.py
+++ microservices/auth_service/app/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 from app.domain.models import User
 from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 # Register your models here.
 admin.site.register(User)
-
 admin.site.register(Permission)
+admin.site.register(ContentType)
