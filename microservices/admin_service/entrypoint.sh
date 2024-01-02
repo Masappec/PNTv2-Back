79c2a9af651624ae36688d02110ad713139b8a94
Anderson Sinaluisa <afullink@gmail.com>
2024-01-01 23:26:02 -0500

    update

--- microservices/admin_service/entrypoint.sh
+++ microservices/admin_service/entrypoint.sh
@@ -1,4 +1,3 @@
 cd code && \
-python3 manage.py migrate && \
-python3 manage.py loaddata fixtures/admin.json && \
+
 gunicorn admin_service.wsgi:application --bind :8001 --workers 2
--- microservices/auth_service/entrypoint.sh
+++ microservices/auth_service/entrypoint.sh
@@ -1,4 +1,3 @@
 cd code && \
-python3 manage.py migrate && \
-python3 manage.py loaddata fixtures/auth.json && \
+
 gunicorn auth_service.wsgi:application --bind :8000 --workers 2
