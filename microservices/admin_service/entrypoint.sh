38cd06cd7918574ff3d6528b2be3ba9eabfda8d5
Anderson Sinaluisa <afullink@gmail.com>
2024-01-02 13:58:37 -0500

    entry

--- microservices/admin_service/entrypoint.sh
+++ microservices/admin_service/entrypoint.sh
@@ -1,5 +1,5 @@
 cd code && \
 # Esperar a que Celery esté listo antes de iniciar Gunicorn
 gunicorn admin_service.wsgi:application --bind :8000 --workers 2 & \
-sleep 5 && \  # Añadir un retardo opcional si es necesario esperar a que Gunicorn se inicie completamente
+sleep 10 && \  # Añadir un retardo opcional si es necesario esperar a que Gunicorn se inicie completamente
 celery -A admin_service worker -l info
--- microservices/auth_service/entrypoint.sh
+++ microservices/auth_service/entrypoint.sh
@@ -3,5 +3,5 @@ cd code && \
 # Esperar a que Celery esté listo antes de iniciar Gunicorn
 # Iniciar Gunicorn primero
 gunicorn auth_service.wsgi:application --bind :8000 --workers 2 & \
-sleep 5 && \  # Añadir un retardo opcional si es necesario esperar a que Gunicorn se inicie completamente
+sleep 10 && \  # Añadir un retardo opcional si es necesario esperar a que Gunicorn se inicie completamente
 celery -A auth_service worker -l info
