9fd7b9039e9e78d4922a899c79c0844622647c17
Anderson Sinaluisa <afullink@gmail.com>
2024-01-02 13:42:55 -0500

    celery sleep

--- microservices/admin_service/entrypoint.sh
+++ microservices/admin_service/entrypoint.sh
@@ -1,3 +1,4 @@
 cd code && \
-celery -A admin_service worker -l info & \
-gunicorn admin_service.wsgi:application --bind :8001 --workers 2
+# Esperar a que Celery esté listo antes de iniciar Gunicorn
+/code/wait-for-it.sh -t 30 celery -A admin_service worker -l info & \
+gunicorn admin_service.wsgi:application --bind :8000 --workers 2
--- microservices/auth_service/entrypoint.sh
+++ microservices/auth_service/entrypoint.sh
@@ -1,4 +1,5 @@
 cd code && \
 
-celery -A auth_service worker -l info & \
+# Esperar a que Celery esté listo antes de iniciar Gunicorn
+/code/wait-for-it.sh -t 30 celery -A auth_service worker -l info & \
 gunicorn auth_service.wsgi:application --bind :8000 --workers 2
