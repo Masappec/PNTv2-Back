272065c959d1f68974c62cd5fe29e474e1189156
Anderson Sinaluisa <afullink@gmail.com>
2024-01-15 15:24:36 -0500

    fix: corrección funciones sql de crear area pedagogica

--- microservices/admin_service/app_admin/migrations/functions/admin_select_pedagogy_area.sql
+++ microservices/admin_service/app_admin/migrations/functions/admin_select_pedagogy_area.sql
@@ -7,16 +7,28 @@ BEGIN
   
 
     -- Devolver el resultado JSONB
+
+    -- Devolver el resultado JSONB
     RETURN jsonb_build_object(
-			'pedagogy', (pa.*),
-			'faq', jsonb_agg(faq.*),
-			'normative', jsonb_agg(nov.*),
-			'tutorial', jsonb_agg(tv.*)
-	)
-	FROM public.app_admin_pedagogyarea pa
-	JOIN public.app_admin_frequentlyaskedquestions faq ON pa.id = faq.pedagogy_area_id
-	LEFT JOIN public.app_admin_normativedocument nov ON pa.id = nov.pedagogy_area_id
-	LEFT JOIN public.app_admin_tutorialvideo tv ON pa.id = tv.pedagogy_area_id
-	WHERE pa.deleted = false GROUP BY pa.id;
+    'pedagogy', (pa.*),
+    'faq', (
+        SELECT jsonb_agg(DISTINCT faq.*)
+        FROM public.app_admin_frequentlyaskedquestions faq
+        WHERE faq.pedagogy_area_id = pa.id
+    ),
+    'normative', (
+        SELECT jsonb_agg(DISTINCT nov.*)
+        FROM public.app_admin_normativedocument nov
+        WHERE nov.pedagogy_area_id = pa.id
+    ),
+    'tutorial', (
+        SELECT jsonb_agg(DISTINCT tv.*)
+        FROM public.app_admin_tutorialvideo tv
+        WHERE tv.pedagogy_area_id = pa.id
+    )
+)
+FROM public.app_admin_pedagogyarea pa
+WHERE pa.deleted = false
+GROUP BY pa.id;
 END;
 $$ LANGUAGE plpgsql;
