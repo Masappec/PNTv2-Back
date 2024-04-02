d396fe939b166ac7e9eb7f655c35ae970a19368f
Anderson Sinaluisa <afullink@gmail.com>
2024-04-02 06:57:00 -0500

    Update main.py

--- microservices/entities_service/entity_app/management/commands/fake_data/ta/main.py
+++ microservices/entities_service/entity_app/management/commands/fake_data/ta/main.py
@@ -14,16 +14,19 @@ class TransparencyActiveFakeData:
     def create_fake_data(self):
         list_establishment = EstablishmentExtended.objects.all()
         numeral_list = self.service.get_all()
+        anios = [2020, 2021, 2022, 2023, 2024]
+        meses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
         for establishment in list_establishment:
-            for numeral in numeral_list:
-
-                self.service.create_transparency(
-                    establishment_id=establishment.id,
-                    numeral_id=numeral.id,
-                    files="",
-                    month=1,
-                    year=2021,
-                    fecha_actual="2021-01-01",
-                    status="ingress"
-                )
+            for anio in anios:
+                for mes in meses:
+                    for numeral in numeral_list:
+                        self.service.create_transparency(
+                            establishment_id=establishment.id,
+                            numeral_id=numeral.id,
+                            files="",
+                            month=mes,
+                            year=anio,
+                            fecha_actual=f"{anio}-{mes}-01",
+                            status="ingress"
+                        )
