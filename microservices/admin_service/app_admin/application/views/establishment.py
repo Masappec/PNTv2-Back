61bb0e993cd2ebfafc2afaad1dfd4804a9a775b6
Anderson Sinaluisa <afullink@gmail.com>
2024-04-17 18:43:14 -0500

    Update establishment.py

--- microservices/admin_service/app_admin/application/views/establishment.py
+++ microservices/admin_service/app_admin/application/views/establishment.py
@@ -224,8 +224,8 @@ class EstablishmentDetail(APIView):
                 'address': establishment.address if establishment.address else '',
                 'type_institution': establishment.type_institution.id if establishment.type_institution else None,
                 'type_organization': establishment.type_organization.id if establishment.type_organization else None,
-                'function_organization': establishment.function_organization.id if establishment.function_organization else None
-
+                'function_organization': establishment.function_organization.id if establishment.function_organization else None,
+                'identification': establishment.identification if establishment.identification else None
 
 
             })
@@ -402,8 +402,8 @@ class EstablismentUpdate(APIView):
                 'address': establishment.address if establishment.address else '',
                 'type_institution': establishment.type_institution.id if establishment.type_institution else None,
                 'type_organization': establishment.type_organization.id if establishment.type_organization else None,
-                'function_organization': establishment.function_organization.id if establishment.function_organization else None
-
+                'function_organization': establishment.function_organization.id if establishment.function_organization else None,
+                'identification': establishment.identification if establishment.identification else ''
 
 
             })
