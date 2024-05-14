1f136fa28b42830950e6d1bdc33c5dc07adf37d8
Anderson Sinaluisa <afullink@gmail.com>
2024-05-14 00:13:52 -0500

    update

--- microservices/public_api_service/core/tasks/ta_tasks.py
+++ microservices/public_api_service/core/tasks/ta_tasks.py
@@ -10,23 +10,26 @@ def on_update_ta(filepaths, date, month, year, user, establishment_identificatio
             with open(filepath, 'r') as file:
                 content = file.read()
 
-                print('Content:', content)
+                csv = pd.DataFrame([x.split(',')
+                                   for x in content.split('\n') if x])
+                data = csv.values.tolist()
+                columns = data.pop(0)
+                print(columns, data)
+                metadata = Metadata(
+                    filename=filepath,
+                    columns=columns,
+                    month=str(month),
+                    year=str(year),
+                    user_upload=str(user),
+                    date_upload=date,
+                    path=filepath,
+                    establishment_identification=establishment_identification,
+
+                )
+                csv_data = CSVData(metadata=metadata, data=data)
+                csv_data.save()
+                print('CSVData saved:', csv_data)
         except Exception as e:
             print('Error reading file:', e)
             continue
-        '''columns = csv.columns.tolist()
-        data = csv.values.tolist()
-
-        metadata = Metadata(
-            filename=filepath,
-            columns=columns,
-            month=month,
-            year=year,
-            user_upload=user,
-            date_upload=date,
-            path=filepath,
-            establishment_identification=establishment_identification
-        )
-        csv_data = CSVData(metadata=metadata, data=data)
-        csv_data.save()
-        print('CSVData saved:', csv_data)'''
+        ''''''
