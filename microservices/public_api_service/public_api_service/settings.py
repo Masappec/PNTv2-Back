3881b16d51af0014261f9c3c5fcc83ff99f5c3db
Anderson Sinaluisa <desarrollo.18@torresytorres.com>
2024-05-31 08:30:27 -0500

    update

--- microservices/public_api_service/public_api_service/settings.py
+++ microservices/public_api_service/public_api_service/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ewoog3=#o@fwuj640mpof=7w!6v_qmk1j1@&z^=0=ulb*zxw6c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
