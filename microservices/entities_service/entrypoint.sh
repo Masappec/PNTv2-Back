457371e8671833ec8942aff407395c0c594d98b8
Anderson Sinaluisa <afullink@gmail.com>
2024-06-01 21:38:08 -0500

    update

--- microservices/entities_service/entrypoint.sh
+++ microservices/entities_service/entrypoint.sh
@@ -3,8 +3,9 @@ export PYTHONPATH=/code:$PYTHONPATH
 # Esperar a que Celery esté listo antes de iniciar Gunicorn
 gunicorn entities_service.wsgi:application --bind :8000 --workers 4 & \
 sleep 10 && \  # Añadir un retardo opcional si es necesario esperar a que Gunicorn se inicie completamente
-celery -A entities_service beat -l info && \ 
-sleep 10 && \ 
 
-celery -A entities_service worker -l info 
+celery -A entities_service worker -l info && \
+celery -A entities_service beat -l info && \ 
+sleep 10 
+
 
