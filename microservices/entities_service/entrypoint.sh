f6a82acff2a298e1be4548c89d5144c14802c704
Anderson Sinaluisa <afullink@gmail.com>
2024-06-02 19:22:30 -0500

    update

--- microservices/entities_service/entrypoint.sh
+++ microservices/entities_service/entrypoint.sh
@@ -1,12 +1,43 @@
-cd code && \
+#!/bin/bash
+
+cd code
 export PYTHONPATH=/code:$PYTHONPATH
-# Esperar a que Celery esté listo antes de iniciar Gunicorn
-gunicorn entities_service.wsgi:application --bind :8000 --workers 4 & \
-sleep 10 && \  # Añadir un retardo opcional si es necesario esperar a que Gunicorn se inicie completamente
 
-celery -A entities_service worker -l info && \
-sleep 10  && \ 
+# Iniciar Gunicorn en segundo plano y esperar a que se inicie
+gunicorn entities_service.wsgi:application --bind :8000 --workers 4 &
+GUNICORN_PID=$!
+sleep 10  # Añadir un retardo para esperar a que Gunicorn se inicie completamente
+
+# Iniciar Celery Worker en segundo plano y esperar a que se inicie
+celery -A entities_service worker -l info &
+CELERY_WORKER_PID=$!
+sleep 10  # Añadir un retardo para esperar a que Celery Worker se inicie completamente
+
+# Iniciar Celery Beat
 celery -A entities_service beat -l info
+CELERY_BEAT_PID=$!
 
+# Función para manejar la terminación de procesos al recibir una señal de interrupción (SIGINT/SIGTERM)
+cleanup() {
+    echo "Deteniendo Gunicorn..."
+    kill $GUNICORN_PID
+    wait $GUNICORN_PID
 
+    echo "Deteniendo Celery Worker..."
+    kill $CELERY_WORKER_PID
+    wait $CELERY_WORKER_PID
 
+    echo "Deteniendo Celery Beat..."
+    kill $CELERY_BEAT_PID
+    wait $CELERY_BEAT_PID
+
+    echo "Todos los procesos han sido detenidos."
+}
+
+# Atrapar señales de interrupción y ejecutar la función cleanup
+trap cleanup SIGINT SIGTERM
+
+# Esperar a que los procesos terminen (mantener el contenedor en ejecución)
+wait $GUNICORN_PID
+wait $CELERY_WORKER_PID
+wait $CELERY_BEAT_PID
