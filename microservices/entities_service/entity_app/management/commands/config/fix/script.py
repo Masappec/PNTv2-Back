865793335c462ee262a50c648cd28edae185dc13
Roberto Esteves <info@robertoesteves.com>
2025-01-24 19:06:10 -0500

    fix-enrique-gallo

--- microservices/entities_service/entity_app/management/commands/config/fix/script.py
+++ microservices/entities_service/entity_app/management/commands/config/fix/script.py
@@ -721,13 +721,13 @@ class ScriptService:
 
     def fix_presidencia(self):
         nu = TransparencyActive.objects.filter(
-            establishment__identification='960002860001',
+            establishment__identification='1768062060001',
             month=10,
             year=2024,
 
         )
         sep = TransparencyActive.objects.filter(
-            establishment__identification='960002860001',
+            establishment__identification='1768062060001',
             month=9,
             year=2024,
         )
