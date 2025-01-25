d060520132279349a33951c2f97a9bfee1139dd0
Roberto Esteves <info@robertoesteves.com>
2025-01-24 19:01:50 -0500

    Update script.py

--- microservices/entities_service/entity_app/management/commands/config/fix/script.py
+++ microservices/entities_service/entity_app/management/commands/config/fix/script.py
@@ -721,13 +721,13 @@ class ScriptService:
 
     def fix_presidencia(self):
         nu = TransparencyActive.objects.filter(
-            establishment__identification='760048700001',
+            establishment__identification='1360064950001',
             month=10,
             year=2024,
 
         )
         sep = TransparencyActive.objects.filter(
-            establishment__identification='760048700001',
+            establishment__identification='1360064950001',
             month=9,
             year=2024,
         )
