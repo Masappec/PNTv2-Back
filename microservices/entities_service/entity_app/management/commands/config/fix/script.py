21df13e9da54d1683a4b0c78ac8b6567fd532998
Roberto Esteves <info@robertoesteves.com>
2025-01-27 09:59:31 -0500

    fix-ineval

--- microservices/entities_service/entity_app/management/commands/config/fix/script.py
+++ microservices/entities_service/entity_app/management/commands/config/fix/script.py
@@ -721,13 +721,13 @@ class ScriptService:
 
     def fix_presidencia(self):
         nu = TransparencyActive.objects.filter(
-            establishment__identification='968563180001',
+            establishment__identification='1768166780001',
             month=10,
             year=2024,
 
         )
         sep = TransparencyActive.objects.filter(
-            establishment__identification='968563180001',
+            establishment__identification='1768166780001',
             month=9,
             year=2024,
         )
